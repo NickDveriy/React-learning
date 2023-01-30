7cdceffe3020336a141399285903ae2dd9d777d6
Mykola Dverii <nick.dveriy95@gmail.com>
2023-01-30 17:42:04 +0200

    REACT-20 Use of NavLink to add additional logic to visited links

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,17 +1,34 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
   return (
     <header className={styles.header}>
-      <ul className={styles.list}>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/products">Products</Link>
-        </li>
-      </ul>
+      <nav>
+        <ul className={styles.list}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => {
+                return isActive ? styles.active : undefined;
+              }}
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/products"
+              className={({ isActive }) => {
+                return isActive ? styles.active : undefined;
+              }}
+            >
+              Products
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
