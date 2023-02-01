7f70722a598df661bde8d8b960372dc6126aa836
Mykola Dverii <nick.dveriy95@gmail.com>
2023-02-01 11:38:58 +0200

    REACT-20.1 Add action() to post new data to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import EventDetailPage, {
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventsRootLayout from "./pages/EventsRoot";
 import HomePage from "./pages/Home";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
             element: <EventsPage />,
             loader: eventsLoader,
           },
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
           {
             path: ":eventId",
             id: "event-detail",
--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,3 +1,4 @@
+import { json, redirect } from "react-router";
 import EventForm from "../components/EventForm";
 
 const NewEventPage = () => {
@@ -5,3 +6,30 @@ const NewEventPage = () => {
 };
 
 export default NewEventPage;
+
+export const action = async ({ request, params }) => {
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  }; // get by name of input specified in form
+
+  console.log("event", eventData);
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    body: JSON.stringify(eventData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Could not save a new event" }, { status: 500 });
+  }
+
+  return redirect("/events");
+};
