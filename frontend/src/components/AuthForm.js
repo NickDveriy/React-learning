9812b118052cef951d599de7e06e225bce9338dc
Mykola Dverii <nick.dveriy95@gmail.com>
2023-02-02 14:18:43 +0200

    REACT-21 Add action to login/signup

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,9 +1,19 @@
-import { Form, Link, useSearchParams } from "react-router-dom";
+import {
+  Form,
+  Link,
+  useSearchParams,
+  useActionData,
+  useNavigation,
+} from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 
 function AuthForm() {
   const [searchParams] = useSearchParams();
+  const data = useActionData();
+  const { state } = useNavigation();
+
+  const isSubmitting = state === "submitting";
 
   const isLogin = searchParams.get("mode") === "login";
 
@@ -11,6 +21,14 @@ function AuthForm() {
     <>
       <Form method="post" className={classes.form}>
         <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
+        {data && data.message && <p>{data.message}</p>}
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
@@ -23,7 +41,9 @@ function AuthForm() {
           <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
             {isLogin ? "Create new user" : "Login"}
           </Link>
-          <button>Save</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Submitting..." : "Save"}
+          </button>
         </div>
       </Form>
     </>
