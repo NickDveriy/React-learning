d362d9a3832e3bc17d28ed990ad97f4e726662f1
Mykola Dverii <nick.dveriy95@gmail.com>
2023-02-03 16:13:57 +0200

    REACT-23 Add link to another page

--- nextjs-course/pages/news/index.js
+++ nextjs-course/pages/news/index.js
@@ -1,7 +1,19 @@
 // our.domain.com/news
 
+import Link from "next/link";
+
 const NewsPage = () => {
-  return <h1>News Page</h1>;
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/some-cool-link">Some coool link</Link>
+        </li>
+        <li>Something else</li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
