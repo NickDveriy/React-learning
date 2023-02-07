b219e219b51349ca6c9ddb171a1a13bb4bca7bca
Mykola Dverii <nick.dveriy95@gmail.com>
2023-02-07 21:59:51 +0200

    REACT-23.1 Add getStaticPaths to define all possible params in dymanic pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,51 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
+  const { image, title, address, description } = props;
   return (
     <MeetupDetail
-      image="https://www.fluentin3months.com/wp-content/uploads/2021/09/language-meetup.jpg"
-      title="A first meetup"
-      address="Some address"
-      description="Meetup description"
+      image={image}
+      title={title}
+      address={address}
+      description={description}
     />
   );
+}
+
+export async function getStaticProps(context) {
+  //fetch a data for a single meetup
+
+  const { meetupId } = context.params;
+
+  return {
+    props: {
+      id: meetupId,
+      image:
+        "https://www.fluentin3months.com/wp-content/uploads/2021/09/language-meetup.jpg",
+      title: "A first meetup",
+      address: "Some address",
+      description: "Meetup description",
+    },
+  };
+}
+
+// to specify all possible param values (meetupId in this case) for dynamic pages
+export function getStaticPaths() {
+  return {
+    fallback: false, // indicate that all supported paths for this page are defined here in paths array
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
 }
 
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const DUMMY_MEETUPS = [
     id: "m2",
     title: "Second meetup",
     image:
-      "https://www.meetup.com/blog/wp-content/uploads/2022/01/pexels-matheus-bertelli-3856033-945x630.jpg",
+      "https://www.fluentin3months.com/wp-content/uploads/2021/09/language-meetup.jpg",
     address: "Some second address",
     description: "Some second description",
   },
