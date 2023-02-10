28b22e6b09d83a22f50d05582b449c17ac7f08fe
Mykola Dverii <nick.dveriy95@gmail.com>
2023-02-10 15:42:01 +0200

    REACT-26 Add additional tests for button click

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 const Greeting = () => {
+  const [isTextChanged, setIsTextChanged] = useState(false);
+
+  const changeTextHandler = () => {
+    setIsTextChanged(true);
+  };
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>Some text</p>
+      {isTextChanged ? <p>Changed text</p> : <p>Initial text</p>}
+      <button onClick={changeTextHandler}>Change text!</button>
     </div>
   );
 };
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,50 @@
 import Greeting from "./Greeting";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test("renders Hello World as a text", () => {
-  //Arrange
-  render(<Greeting />);
+describe("Greeting component", () => {
+  test("renders 'Hello World' as a text", () => {
+    //Arrange
+    render(<Greeting />);
 
-  //Act
-  // ... nothing
+    //Act
+    // ... nothing
 
-  //Assert
-  const element = screen.getByText("Hello World", { exact: false });
-  expect(element).toBeInTheDocument();
+    //Assert
+    const element = screen.getByText("Hello World", { exact: false });
+    expect(element).toBeInTheDocument();
+  });
+
+  test("renders 'Initial text' if the button was not clicked", () => {
+    render(<Greeting />);
+
+    const textElement = screen.getByText("Initial text");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test("renders 'Changed text' if the button was clicked", () => {
+    render(<Greeting />);
+
+    // const button = screen.getByText("Change text!");
+    // button.click();
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    const textElement = screen.getByText("Changed text");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test("does not render 'Initial text' if the button was clicked", () => {
+    render(<Greeting />);
+
+    // const button = screen.getByText("Change text!");
+    // button.click();
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    const textElement = screen.queryByText("Initial text");
+    expect(textElement).toBeNull();
+  });
 });
